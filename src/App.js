289a2d03eac4523b7e8c609276abf6ce44d3c957
pruthvi <pruthvirajbarbole55@gmail.com>
2024-03-21 18:30:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+      const token = "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";
+     
+      const [inputData ,setInputData] =useState("");
+      const [imgSrc, setImgSrc] = useState("");
+    
+      async function query() {
+        try {
+            const response = await fetch(
+                "https://api-inference.huggingface.co/models/Melonie/text_to_image_finetuned", {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                },
+                method: "POST",
+                body: JSON.stringify({
+                    "inputs": inputData
+                }),
+            });
+            const result = await response.blob();
+            return result;
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    }
+
+     
+    async function handleClick() {
+      try {
+          const response = await query();
+          const convertToObjectUrl = URL.createObjectURL(response);
+          setImgSrc(convertToObjectUrl);
+      } catch (error) {
+          console.error("Error:", error);
+      }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Image Genrater App</h1>
+        <input type="text" value={inputData} onChange={(e) => setInputData(e.target.value)} placeholder="Type here..." />
+        <button onClick={handleClick}>Search</button>
+        <img id="imglk" src={imgSrc} alt="" />
     </div>
-  );
+   );
 }
-
 export default App;
